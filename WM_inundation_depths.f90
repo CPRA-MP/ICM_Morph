2953dc2ad059e587296469cffd5e6e87845f8b90
Eric <47019383+edwhite12@users.noreply.github.com>
2020-08-17 20:49:59 -0500

    compiler test

--- WM_inundation_depths.f90
+++ WM_inundation_depths.f90
@@ -54,7 +54,7 @@ subroutine inundation_depths
         
         dem_inun_dep(i,tp) = wse - z
         if (wse > z) then        
-            write(*,*) comp_ndem_wet(1,1)      
+            write(*,*) comp_ndem_wet(946,1)      
             !comp_ndem_wet(c,tp) = comp_ndem_wet(c,tp) + 1
             !grid_ndem_wet(g,tp) = grid_ndem_wet(g,tp)  + 1
         end if
