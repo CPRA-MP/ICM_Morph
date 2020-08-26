5c8603910ccc067f76ec5fcf87e4f70240c0691b
Eric <47019383+edwhite12@users.noreply.github.com>
2020-08-26 15:56:56 -0500

    Update WM_write_output_binary_rasters.f90

--- WM_write_output_binary_rasters.f90
+++ WM_write_output_binary_rasters.f90
@@ -37,30 +37,51 @@ subroutine write_output_binary_rasters
     open(unit=804, file = trim(adjustL(dz_eoy_xyz_file))//'.b',form='unformatted')
     write(804) dem_dz_cm
     close(804)    
+
+    if (elapsed_year == 1) then
     
-    write(  *,*) ' - writing output raster binary file for X-coordinate'
-    write(000,*) ' - writing output raster binary file for X-coordinate'
-    open(unit=805, file = trim(adjustL('geomorph/output/raster_x_coord.b')),form='unformatted')
-    write(805) dem_x
-    close(805)  
-    
-    write(  *,*) ' - writing output raster binary file for Y-coordinate'
-    write(000,*) ' - writing output raster binary file for Y-coordinate'
-    open(unit=806, file = trim(adjustL('geomorph/output/raster_y_coord.b')),form='unformatted')
-    write(806) dem_y
-    close(806)  
-    
-    write(  *,*) ' - writing output raster binary file for compartment map'
-    write(000,*) ' - writing output raster binary file for compartment map'
-    open(unit=807, file=trim(adjustL(comp_file))//'.b',form='unformatted')
-    write(807) dem_comp
-    close(807)
-    
-    write(  *,*) ' - writing output raster binary file for grid map'
-    write(000,*) ' - writing output raster binary file for grid map'
-    open(unit=808, file=trim(adjustL(grid_file))//'.b',form='unformatted')
-    write(808) dem_grid
-    close(808)
+        write(  *,*) ' - writing output raster binary file for X-coordinate'
+        write(000,*) ' - writing output raster binary file for X-coordinate'
+        open(unit=805, file = trim(adjustL('geomorph/output/raster_x_coord.b')),form='unformatted')
+        write(805) dem_x
+        close(805)  
+        
+        write(  *,*) ' - writing output raster binary file for Y-coordinate'
+        write(000,*) ' - writing output raster binary file for Y-coordinate'
+        open(unit=806, file = trim(adjustL('geomorph/output/raster_y_coord.b')),form='unformatted')
+        write(806) dem_y
+        close(806)  
+        
+        write(  *,*) ' - writing output raster binary file for compartment map'
+        write(000,*) ' - writing output raster binary file for compartment map'
+        open(unit=807, file=trim(adjustL(comp_file))//'.b',form='unformatted')
+        write(807) dem_comp
+        close(807)
+        
+        write(  *,*) ' - writing output raster binary file for grid map'
+        write(000,*) ' - writing output raster binary file for grid map'
+        open(unit=808, file=trim(adjustL(grid_file))//'.b',form='unformatted')
+        write(808) dem_grid
+        close(808)
+
+        write(  *,*) ' - writing output raster binary file for marsh edge erosion rate'
+        write(000,*) ' - writing output raster binary file for marsh edge erosion rate'
+        open(unit=809, file=trim(adjustL(meer_file))//'.b',form='unformatted')
+        write(809) dem_meer
+        close(809)
+        
+        write(  *,*) ' - writing output raster binary file for polder areas'
+        write(000,*) ' - writing output raster binary file for polder areas'
+        open(unit=810, file=trim(adjustL(pldr_file))//'.b',form='unformatted')
+        write(810) dem_pldr
+        close(810)
+        
+        write(  *,*) ' - writing output raster binary file for polder areas'
+        write(000,*) ' - writing output raster binary file for polder areas'
+        open(unit=811, file=trim(adjustL(dsub_file))//'.b',form='unformatted')
+        write(811) dem_dpsb
+        close(811)
+    end if
     
     return
 end
