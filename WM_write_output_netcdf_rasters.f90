b7266c2c8f7c00b58e3669ea608dfc2ff6092165
Eric <47019383+edwhite12@users.noreply.github.com>
2020-09-03 16:50:34 -0500

    test NC writing

--- WM_write_output_netcdf_rasters.f90
+++ WM_write_output_netcdf_rasters.f90
@@ -8,26 +8,31 @@ subroutine write_output_netcdf_rasters
     
     write(  *,*) ' - writing output raster XYZ file for Edge'
     write(000,*) ' - writing output raster XYZ file for Edge'
-    call write_netcdf_float(trim(adjustL('output/'//edge_eoy_xyz_file//'.nc')),&
-                                   dem_x,dem_y,dem_z,dble(dem_NoDataVal),"edge")
+    call write_netcdf_int(trim(adjustL('output/'//grid_file//'.nc')),&
+                                   dem_x,dem_y,dem_grid,dble(dem_NoDataVal),"gridID")
 
-    write(  *,*) ' - writing output raster XYZ file for landchange flag'
-    write(000,*) ' - writing output raster XYZ file for landchange flag'
-    call write_netcdf_int(trim(adjustL('output/'//lndchng_eoy_xyz_file)),&
-                                   dem_x,dem_y,lnd_change_flag,&
-                                   dble(dem_NoDataVal),"landchange")
-    
-    write(  *,*) ' - writing output raster XYZ file for land type'
-    write(000,*) ' - writing output raster XYZ file for land type'
-    call write_netcdf_int(trim(adjustL('output/'//lndtyp_eoy_xyz_file)),&
-                                   dem_x,dem_y,dem_lndtyp,&
-                                   dble(dem_nodataval),"landtype")
-    
-    write(  *,*) ' - writing output raster XYZ file for elevation change'
-    write(000,*) ' - writing output raster XYZ file for elevation change'
-    call write_netcdf_float(trim(adjustL('output/'//dz_eoy_xyz_file)),&
-                                   dem_x,dem_y,dem_dz_cm,dble(dem_nodataval),&
-                                   "elevationchange")
+!    write(  *,*) ' - writing output raster netCDF for topobathy DEM'
+!    write(000,*) ' - writing output raster netCDF for topobathy DEM'
+!    call write_netcdf_float(trim(adjustL('output/'//dem_eoy_xyz_file//'.nc')),&
+!                                   dem_x,dem_y,dem_z,dble(dem_NoDataVal),"z")
+!
+!    write(  *,*) ' - writing output raster netCDF for landchange flag'
+!    write(000,*) ' - writing output raster netCDF for landchange flag'
+!    call write_netcdf_int(trim(adjustL('output/'//lndchng_eoy_xyz_file)),&
+!                                   dem_x,dem_y,lnd_change_flag,&
+!                                   dble(dem_NoDataVal),"landchange")
+!    
+!    write(  *,*) ' - writing output raster netCDF for land type'
+!    write(000,*) ' - writing output raster netCDF for land type'
+!    call write_netcdf_int(trim(adjustL('output/'//lndtyp_eoy_xyz_file)),&
+!                                   dem_x,dem_y,dem_lndtyp,&
+!                                   dble(dem_nodataval),"landtype")
+!    
+!    write(  *,*) ' - writing output raster netCDF for elevation change'
+!    write(000,*) ' - writing output raster netCDF for elevation change'
+!    call write_netcdf_float(trim(adjustL('output/'//dz_eoy_xyz_file)),&
+!                                   dem_x,dem_y,dem_dz_cm,dble(dem_nodataval),&
+!                                   "elevationchange")
     
     return
 end subroutine write_output_netcdf_rasters
