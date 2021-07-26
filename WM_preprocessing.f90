95a45fd43e9304bba0968bf78738bb22b9a246a9
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-07-26 14:34:32 -0500

    Update WM_preprocessing.f90

--- WM_preprocessing.f90
+++ WM_preprocessing.f90
@@ -199,10 +199,11 @@ subroutine preprocessing
     do i = 1,ncomp
         read(1115,*) dump_int,                          &       ! ICM-Hydro_comp
    &                 comp_eco(i),                       &       ! ecoregion number 
-   &                 dump_txt,                          &       ! ecoregion code
-   &                 dump_txt,                          &       ! descriptive name
    &                 comp_land_outside_grid_m2(i),      &       ! area of compartment that is outside of ICM-LAVegMod grid and is assumed to always be land
-   &                 comp_watr_outside_grid_m2(i)               ! area of compartment that is outside of ICM-LAVegMod grid and is assumed to always be water
+   &                 comp_watr_outside_grid_m2(i),      &       ! area of compartment that is outside of ICM-LAVegMod grid and is assumed to always be water
+   &                 dump_txt,                          &       ! ecoregion code
+   &                 dump_txt                                   ! descriptive name
+
         comp_land_outside_grid_pixels(i) = int(comp_land_outside_grid_m2(i))/dem_res**2
         comp_watr_outside_grid_pixels(i) = int(comp_watr_outside_grid_m2(i))/dem_res**2
     end do
