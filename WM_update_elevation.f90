98f9e91ec08f9fce2d03bd5160710802e34d6b7b
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-08-13 16:38:25 -0500

    correction for lowering dead flotant
    also added comments

--- WM_update_elevation.f90
+++ WM_update_elevation.f90
@@ -32,42 +32,47 @@ subroutine update_elevation
             c = dem_comp(i)
             if (c /= dem_NoDataVal) then
                 if (dem_to_bidem(i) == dem_NoDataVal) then                                                                      ! if pixel is not within barrier island domain            
-                    if (dem_bg_flag(i) == 1) then                                                                               ! lower old bareground pixels
+                    if (dem_bg_flag(i) == 1) then                                                                               
                         dz_cm_lndtyp = -1.0*bg_lowerZ_m*100.0
                     else 
-                        if (lnd_change_flag(i) == -2) then                                                                      ! lower dead flotant pixels to 1 m below mean water level
-                            dz_cm_lndtyp = min(0.00,-100.0*( dem_z(i) - (stg_av_yr(c) - flt_lowerDepth_m) ))                    ! if current elevation is already more than 1-m below water level, do not allow for elevation gain
-                        else if (lnd_change_flag(i) == -3) then                                                                 ! lower eroded edge pixels to  25 cm below mean water level
+                        if (lnd_change_flag(i) == -2) then                                                                      ! lower dead flotant pixels (lnd_change = -2) to a given depth below mean water level
+                            dz_cm_lndtyp = min(0.00,-100.0*( dem_z(i) - (stg_av_yr(c) - flt_lowerDepth_m) ))                    ! if current elevation is already lower than this depth do not allow for elevation gain
+                        else if (lnd_change_flag(i) == -3) then                                                                 ! lower eroded edge pixels (lnd_change = -3) to  25 cm below mean water level
                             dz_cm_lndtyp = min(0.00,-100.0*( dem_z(i) - (stg_av_yr(c) - me_lowerDepth_m) )) 
                         end if
                     end if
                 
                     er = comp_eco(c)            
                     if (er /= dem_NoDataVal) then
+                        !! vegetated land
                         if (dem_lndtyp(i) == 1) then
-                            if (dem_pldr(i) == 1) then                                                                          ! poldered vegetated land dz_cm = f(deep)
-                                dz_cm = 0.0 - dem_dpsb(i)/10.0 + dz_cm_lndtyp                                                   ! convert subsidence from mm/yr to cm/yr
-                            elseif (lnd_change_flag(i) == 0) then                                                                  ! non-polder vegetated land dz_cm = f(organic, mineral, deep, shallow),
-                                dz_cm = org_accr_cm(i) + min_accr_cm(i) - dem_dpsb(i)/10.0 - er_shsb(er,ssub_col)/10.0 + dz_cm_lndtyp    ! convert subsidence from mm/yr to cm/yr
-                            else
-                                dz_cm = min_accr_cm(i) - dem_dpsb(i)/10.0 - er_shsb(er,ssub_col)/10.0 + dz_cm_lndtyp
+                            if (dem_pldr(i) == 1) then                                                                                  ! poldered vegetated land dz_cm = f(deep)
+                                dz_cm = 0.0 - dem_dpsb(i)/10.0 + dz_cm_lndtyp                                                           ! convert subsidence from mm/yr to cm/yr
+                            else if (lnd_change_flag(i) == 0) then                                                                      ! non-polder vegetated land with no land change dz_cm = f(organic, mineral, deep, shallow)
+                                dz_cm = org_accr_cm(i) + min_accr_cm(i) - dem_dpsb(i)/10.0 - er_shsb(er,ssub_col)/10.0 + dz_cm_lndtyp   
+                            else                                                                                                        ! non-polder vegetated land with land change (meaning was vegetated now water) - do not include organic accretion given land loss occurred, but still update for subsidence    
+                                dz_cm = min_accr_cm(i) - dem_dpsb(i)/10.0 - er_shsb(er,ssub_col)/10.0 + dz_cm_lndtyp                
                             end if
-                        else if (dem_lndtyp(i) == 2) then                                                                       ! water bottom dz_cm = f(mineral, deep)
-                            dz_cm = min_accr_cm(i) - dem_dpsb(i)/10.0 + dz_cm_lndtyp                                            ! convert subsidence from mm/yr to cm/yr
-                        else if (dem_lndtyp(i) == 3) then                                                                       ! nonvegetated wetland dz_cm = f(mineral, deep, shallow)
-                            dz_cm = min_accr_cm(i) - dem_dpsb(i)/10.0 - er_shsb(er,ssub_col)/10.0  + dz_cm_lndtyp                        ! convert subsidence from mm/yr to cm/yr
-                        else if (dem_lndtyp(i) == 4) then                                                                       ! upland/developed dz_cm = f(deep)
-                            dz_cm = 0.0 - dem_dpsb(i)/10.0 + dz_cm_lndtyp                                                       ! convert subsidence from mm/yr to cm/yr
-                        else                                                                                                    ! flotant dz_cm = 0
-                            dz_cm = 0.0
+                        !! water bottom
+                        else if (dem_lndtyp(i) == 2) then                                                                               ! water bottom dz_cm = f(mineral, deep)
+                            dz_cm = min_accr_cm(i) - dem_dpsb(i)/10.0 + dz_cm_lndtyp                                                    
+                        !! bare ground
+                        else if (dem_lndtyp(i) == 3) then                                                                               ! nonvegetated wetland dz_cm = f(mineral, deep, shallow)
+                            dz_cm = min_accr_cm(i) - dem_dpsb(i)/10.0 - er_shsb(er,ssub_col)/10.0  + dz_cm_lndtyp                       
+                        !! upland/developed
+                        else if (dem_lndtyp(i) == 4) then                                                                               ! upland/developed dz_cm = f(deep)
+                            dz_cm = 0.0 - dem_dpsb(i)/10.0 + dz_cm_lndtyp                                                               
+                        !! flotant marsh
+                        else if (dem_lndtyp(i) == 5) then                                                                               ! flotant marsh  dz_cm will only be non-zero if flotant is dead and converts to open water (lnd_change = -2) (value set above)
+                            dz_cm = dz_cm_lndtyp                                                                                        
                         end if 
                     end if
                     
-                    dem_z(i) = dem_z(i) + dz_cm/100.0                                                                           ! dz_cm is in cm, DEM is in meters
-                    dem_dz_cm(i) = dz_cm                                                                                        ! update dZ array for writing output file
+                    dem_z(i) = dem_z(i) + dz_cm/100.0                                                                                   ! dz_cm is in cm, DEM is in meters
+                    dem_dz_cm(i) = dz_cm                                                                                                ! update dZ array for writing output file
                 
                 else        
-                    dem_z(i) = dem_z_bi(dem_to_bidem(i))                                                                        ! if in BI-DEM domain,, use ICM-BI-DEM elevation as final elevation
+                    dem_z(i) = dem_z_bi(dem_to_bidem(i))                                                                                ! if in BI-DEM domain,, use ICM-BI-DEM elevation as final elevation
                 end if
             end if
         end if
