17eb6ff70688259a1a0d2a26223d28df32ce3e5b
Eric <47019383+edwhite12@users.noreply.github.com>
2021-11-01 12:13:26 -0500

    MC projects done

--- WM_build_projects.f90
+++ WM_build_projects.f90
@@ -20,10 +20,12 @@ subroutine build_projects
     integer :: prj_dem_y                                                ! local variable to store Y-coordinates of project DEM
     real(sp) :: design_elev                                             ! temporary variable to store the calculated design elevation for marsh creation projects to convert from MWL datum to NAVD88
     real(sp),dimension(:),allocatable :: prj_dem_z                      ! project design elevation of DEM pixel (m NAVD88) - for ridge/levee projects and meters above MWL for marsh creation projects
-
+    real(sp) :: element_volume_m3                                       ! cumulative sediment volume needed to build current ElementID of project
+    real(sp) :: element_footprint_m2                                    ! cumulative sediment volume needed to build current ElementID of project
+    
     
     allocate (prj_dem_z(ndem))
-    prj_dem_z = 0
+    prj_dem_z = dem_NoDataVal           ! intialize project elevation raster to NoData
     
     write(  *,*) ' - implementing FWA projects'
     write(000,*) ' - implementing FWA projects'
@@ -34,23 +36,27 @@ subroutine build_projects
         write(000,'(A,I,A)') '    - ',n_mc,' marsh creation projects being implemented'
         
         open(unit=401, file=trim(adjustL(project_list_MC_file)))
-        read(401,*) dump_txt        ! dump header
+        read(401,*) dump_txt            ! dump header
+        
+        open(unit=402, file=trim(adjustL(project_list_MCva_file)))
+        write(402,'(A)') 'ElementID,SedimentVolume_m3,ProjectFootprint_m2'
         
         do imc = 1,n_mc
-            prj_dem_z = 0               ! intialize project elevation raster to 0
-            design_elev = -9999         ! initialize design elevation to NoData value
+            prj_dem_z = dem_NoDataVal           ! intialize project elevation raster to NoData
+            design_elev = -9999                 ! initialize design elevation to NoData value used in project DEM files
             
-            read(401,*) prj_xyz_file                                                ! read filepath to XYZ raster
-            write(  *,'(A,A)') '      - building from XYZ file:',prj_xyz_file
-            write(000,'(A,A)') '      - building from XYZ file:',prj_xyz_file
+            read(401,*) ElementID,prj_xyz_file  ! ElementID and filepath to XYZ raster
+            write(  *,'(A,I,A,A)') '      - building ',ElementID,' from XYZ file:',prj_xyz_file
+            write(000,'(A,I,A,A)') '      - building ',ElementID,' from XYZ file:',prj_xyz_file
             
-            open(402,  file=trim(adjustL(prj_xyz_file)))                            ! open XYZ file for specific project element
+            ! open XYZ file for specific project element
+            open(403,  file=trim(adjustL(prj_xyz_file)))
             
-
-            do i = 1,ndem                                                        ! read in project DEM
-                read(402,*) prj_dem_x,prj_dem_y,prj_dem_z(i)
-                    
-                if (prj_dem_x /= dem_x(i)) then                                 ! check that coordinates of project DEM match the initial DEM used by the model - exit if they do not match structure
+            ! read in project DEM
+            do i = 1,ndem
+                read(403,*) prj_dem_x,prj_dem_y,prj_dem_z(i)
+                ! check that coordinates of project DEM match the initial DEM used by the model - exit if they do not match structure
+                if (prj_dem_x /= dem_x(i)) then
                     write(  *,'(A,I,A,I)') ' !!! EXITING !!! project XYZ file X value does not match input DEM structure, dem_x = ', dem_x(i), ', prj_dem_x = ', prj_dem_x
                     write(000,'(A,I,A,I)') ' !!! EXITING !!! project XYZ file X value does not match input DEM structure, dem_x = ', dem_x(i), ', prj_dem_x = ', prj_dem_x
                     stop
@@ -73,20 +79,41 @@ subroutine build_projects
                         prj_dem_z(i) = design_elev  ! update project elevation raster to use the design elevation rather than the height above mean water level
                     end if
                 end if
-                
-                
-                
-                
             end do
-            close(402)
-                
+            close(403)
+            ! done reading in project DEM
             
+            ! update elevation and land type for project footprint
+            element_volume_m3 = 0.0
+            element_footprint_m2 = 0.0
             do i = 1,ndem
-
+                if (prj_dem_z(i) /= dem_NoDataVal) then
+                    c = dem_comp(i)
+                    if (c /= dem_NoDataVal) then
+                        depth = ( stg_av_yr(c) - dem_z(i) )     ! negative depth values mean elevation is greater than mean water level
+                    end if
+                    if (depth <= mc_depth_threshold) then
+                        
+                        proj_dz_m = prj_dem_z(i) - dem_z(i)     ! calculate change in pixel elevation due to project
+                        dem_dz_cm(i) = prj_dz_m*100.0           ! update dZ raster for project implementation *note that for pixels under a project, the dZ raster will be calculated from the end-of-year raster that already has subsidence and accretion update added for the year
+                        
+                        if (dem_lndtyp(i) == 2) then            ! if pixel originally water, set land change flag to new land
+                            lnd_change_flag(i) == 1
+                        end 
+                        
+                        dem_z(i) = prj_dem_z(i)                 ! set pixel elevation to project elevation
+                        dem_lndtyp(i) = 3                       ! set pixel landtype to bare ground/unvegetated wetland
+                        
+                        element_volume_m3 = element_volume_m3 + proj_dz_m*dem_res*dem_res
+                        element_footprint_m2 = element_footprint_m2 + dem_res*dem_res
+                    end if
+                end if
             end do
-        
-        end do        
-        
+            ! write summary volumes and footprints to output file for MC projects
+            write(402, '(I,x,F0.4,x,I)') elementID,',',element_volume_m3,',',element_footprint_m2
+        end do    
+                
+        close(402)
         close(401)
     else
         write(  *,*) '    - no marsh creation projects in this run'
