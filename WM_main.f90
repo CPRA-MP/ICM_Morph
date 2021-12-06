33c9c856c349ceee399c339fd1abd5828fcd037e
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-12-06 16:06:11 -0600

    Update WM_main.f90

--- WM_main.f90
+++ WM_main.f90
@@ -75,118 +75,15 @@ program main
     write(  *,*) 'Inundation Depths subroutine ended at: ',dtstr
     write(000,*) 'Inundation Depths subroutine ended at: ',dtstr
 
-    
-    call edge_delineation
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Edge Delineation subroutine ended at: ',dtstr
-    write(000,*) 'Edge Delineation subroutine ended at: ',dtstr
-
-    
-    call mineral_deposition
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Mineral Deposition subroutine ended at: ',dtstr
-    write(000,*) 'Mineral Deposition subroutine ended at: ',dtstr
-
-    
-    call organic_accretion
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Organic Accretion subroutine ended at: ',dtstr
-    write(000,*) 'Organic Accretion subroutine ended at: ',dtstr
-
-
-    lnd_change_flag = 0                             ! initialize land change flag for each DEM pixel to zero  
-    
-    call flotant
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Flotant Marsh subroutine ended at: ',dtstr
-    write(000,*) 'Flotant Marsh subroutine ended at: ',dtstr
- 
-    
-    call build_shoreline_projects
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Build Shoreline Protection Projects subroutine ended at: ',dtstr
-    write(000,*) 'Build Shoreline Protection Projects subroutine ended at: ',dtstr    
-    
-    
-    call edge_erosion
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Edge Erosion subroutine ended at: ',dtstr
-    write(000,*) 'Edge Erosion subroutine ended at: ',dtstr    
-
-    
-    call map_bareground
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Map Bareground subroutine ended at: ',dtstr
-    write(000,*) 'Map Bareground subroutine ended at: ',dtstr    
- 
-    
-    call inundation_thresholds
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Inundation Thresholds subroutine ended at: ',dtstr
-    write(000,*) 'Inundation Thresholds subroutine ended at: ',dtstr    
-    
    
-    call update_elevation
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Update Elevation subroutine ended at: ',dtstr
-    write(000,*) 'Update Elevation subroutine ended at: ',dtstr       
 
-    
-    call new_subaerial_land
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'New Subaerial Land subroutine ended at: ',dtstr
-    write(000,*) 'New Subaerial Land subroutine ended at: ',dtstr   
-    
-   
-    call update_landtype
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Update Landtype subroutine ended at: ',dtstr
-    write(000,*) 'Update Landtype subroutine ended at: ',dtstr    
-
-    
     call inundation_HSI_bins
     call date_and_time(VALUES=dtvalues)
     write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
     write(  *,*) 'HSI Inundation subroutine ended at: ',dtstr
     write(000,*) 'HSI Inundation subroutine ended at: ',dtstr    
     
-    
-!    call distance_to_land
-!    call date_and_time(VALUES=dtvalues)
-!    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-!    write(  *,*) 'Distance-to-land subroutine ended at: ',dtstr
-!    write(000,*) 'Distance-to-land subroutine ended at: ',dtstr 
-!    
-!    call sav
-!    call date_and_time(VALUES=dtvalues)
-!    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-!    write(  *,*) 'SAV subroutine ended at: ',dtstr
-!    write(000,*) 'SAV subroutine ended at: ',dtstr 
-    
-    call build_marsh_projects
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Build Marsh Projects subroutine ended at: ',dtstr
-    write(000,*) 'Build Marsh Projects subroutine ended at: ',dtstr  
-    
-    call build_ridge_projects
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Build Ridge (and Levee) Projects subroutine ended at: ',dtstr
-    write(000,*) 'Build Ridge (and Levee) Projects subroutine ended at: ',dtstr  
-    
-    
+
     call summaries
     call date_and_time(VALUES=dtvalues)
     write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
@@ -200,33 +97,6 @@ program main
     write(  *,*) 'Write Output subroutine ended at: ',dtstr
     write(000,*) 'Write Output subroutine ended at: ',dtstr    
 
-    
-    call write_output_QAQC_points
-    call date_and_time(VALUES=dtvalues)
-    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    write(  *,*) 'Write Output subroutine ended at: ',dtstr
-    write(000,*) 'Write Output subroutine ended at: ',dtstr  
-    
-    
-    if (binary_out == 1) then    
-        call write_output_binary_rasters
-        call date_and_time(VALUES=dtvalues)
-        write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-        write(  *,*) 'Write Binary Raster Output subroutine ended at: ',dtstr
-        write(000,*) 'Write Binary Raster Output subroutine ended at: ',dtstr    
-    !else if(binary_out == 2)then
-    !    call write_output_netcdf_rasters
-    !    call date_and_time(VALUES=dtvalues)
-    !    write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-    !    write(  *,*) 'Write netCDF Raster Output subroutine ended at: ',dtstr
-    !    write(000,*) 'Write netCDF Raster Output subroutine ended at: ',dtstr    
-    else
-        call write_output_asci_rasters
-        call date_and_time(VALUES=dtvalues)
-        write(dtstr,8889) dtvalues(1),'-',dtvalues(2),'-',dtvalues(3),' ',dtvalues(5),':',dtvalues(6),':',dtvalues(7)
-        write(  *,*) 'Write ASCI Raster Output subroutine ended at: ',dtstr
-        write(000,*) 'Write ASCI Raster Output subroutine ended at: ',dtstr 
-    end if
 
     write(  *,*)
     write(  *,*) 'Ended ICM-Morph simulation at: ',dtstr
