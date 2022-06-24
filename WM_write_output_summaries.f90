3fa08ff6239c1ab98108892f44ff5abd7ff16cfe
Eric White <47019383+edwhite12@users.noreply.github.com>
2022-06-24 10:58:39 -0500

    update for external zonal statistics

--- WM_write_output_summaries.f90
+++ WM_write_output_summaries.f90
@@ -13,26 +13,18 @@ subroutine write_output_summaries
     real(sp) :: grid_pct_land_tot                                   ! calculation of total land area, regardless of landtype (excludes flotant and water)
     real(sp) :: grid_pct_land_wetl                                  ! calculation wetland area, either attached or flotant - with or without vegetation (excludes water and upland)
     character*3,dimension(:),allocatable :: lnd_codes               ! array of land-type codes used in output ecoregion summary file
+    integer :: write_debug                                          ! flag to indicate whether debug files are to be written (1) or not (0)
     
+    write_zonal_stats = 0       ! do not write zonal stats grid and comp files - these will be written by ICM.py using
+    write_debug = 0             ! do not write debug files
     
     ! write end-of-year grid summary file    
     write(  *,*) ' - writing end-of-year grid summary files'
     write(000,*) ' - writing end-of-year grid summary files'
     
     open(unit=900, file = trim(adjustL(grid_summary_eoy_file) ))
-    open(unit=901, file = trim(adjustL(grid_data_file) ))
-    open(unit=902, file = trim(adjustL(grid_pct_edge_file) ))
-    open(unit=903, file = trim(adjustL(grid_depth_file_Gdw) ))
-    open(unit=904, file = trim(adjustL(grid_depth_file_GwT) ))
-    open(unit=905, file = trim(adjustL(grid_depth_file_MtD) ))
-    
-    ! write headers
+    ! open and write headers
     write(900,'(A)') 'gridID,compID,stage_m_NAVD88,stg_wlv_smr_m,mean_sal_ppt,mean_smr_sal_ppt,2wk_max_sal_ppt,mean_temp_degC,mean_TSS_mgL,pct_water,pct_flotant,pct_land_veg,pct_land_bare,pct_land_upland_dry,pct_land_upland_wet,pct_vglnd_BLHF,pct_vglnd_SWF,pct_vglnd_FM,pct_vglnd_IM,pct_vglnd_BM,pct_vglnd_SM,FIBS_score'
-    write(901,'(A)') 'GRID,MEAN_BED_ELEV,MEAN_LAND_ELEV,PERCENT_LAND_0-100,PERCENT_WETLAND_0-100,PERCENT_WATER_0-100'
-    write(902,'(A)') 'GRID,PERCENT_EDGE_0-100'
-    write(903,'(A)') 'GRID_ID,VALUE_0,VALUE_4,VALUE_8,VALUE_12,VALUE_18,VALUE_22,VALUE_28,VALUE_32,VALUE_36,VALUE_40,VALUE_44,VALUE_78,VALUE_150,VALUE_151'
-    write(904,'(A)') 'GRID_ID,VALUE_0,VALUE_6,VALUE_18,VALUE_22,VALUE_26,VALUE_30,VALUE_34,VALUE_100,VALUE_101'
-    write(905,'(A)') 'GRID_ID,VALUE_0,VALUE_8,VALUE_30,VALUE_36,VALUE_42,VALUE_46,VALUE_50,VALUE_56,VALUE_57'
  
     do i = 1,ngrid
         c = grid_comp(i)
@@ -68,115 +60,131 @@ subroutine write_output_summaries
    &                grid_pct_vglnd_SM(i),           &
    &                grid_FIBS_score(i)
        
-
-
-        ! Grid data file passed into ICM-Hydro reports percentages from 0-100 instead of 0-1
-        write(901,1901) i,                          &
-   &                grid_bed_z(i),                  &
-   &                grid_land_z(i),                 &
-   &                100.0*grid_pct_land_tot,        &
-   &                100.0*grid_pct_land_wetl,       &
-   &                100.0*grid_pct_water(i)
-        
-        ! Grid based edge file passed into ICM-HSI reports percentages from 0-100 instead of 0-1    
-        write(902,1902) i,                          &
-   &                100.0*grid_pct_edge(i)
-        
-        write(903,1903) i,                          &
-   &                grid_gadwl_dep(i,1),            &
-   &                grid_gadwl_dep(i,2),            &
-   &                grid_gadwl_dep(i,3),            &
-   &                grid_gadwl_dep(i,4),            &
-   &                grid_gadwl_dep(i,5),            &
-   &                grid_gadwl_dep(i,6),            &
-   &                grid_gadwl_dep(i,7),            &
-   &                grid_gadwl_dep(i,8),            &
-   &                grid_gadwl_dep(i,9),            &
-   &                grid_gadwl_dep(i,10),           &
-   &                grid_gadwl_dep(i,11),           &
-   &                grid_gadwl_dep(i,12),           &
-   &                grid_gadwl_dep(i,13),           &
-   &                grid_gadwl_dep(i,14)
-        
-        write(904,1904) i,                          &
-   &                grid_gwteal_dep(i,1),           &
-   &                grid_gwteal_dep(i,2),           &
-   &                grid_gwteal_dep(i,3),           &
-   &                grid_gwteal_dep(i,4),           &
-   &                grid_gwteal_dep(i,5),           &
-   &                grid_gwteal_dep(i,6),           &
-   &                grid_gwteal_dep(i,7),           &
-   &                grid_gwteal_dep(i,8),           &
-   &                grid_gwteal_dep(i,9)
-        
-        write(905,1904) i,                          &
-   &                grid_motduck_dep(i,1),          &
-   &                grid_motduck_dep(i,2),          &
-   &                grid_motduck_dep(i,3),          &
-   &                grid_motduck_dep(i,4),          &
-   &                grid_motduck_dep(i,5),          &
-   &                grid_motduck_dep(i,6),          &
-   &                grid_motduck_dep(i,7),          &
-   &                grid_motduck_dep(i,8),          &
-   &                grid_motduck_dep(i,9)
-    
     end do
-    
     close(900)
-    close(901)
-    close(902)
-    close(903)
-    close(904)
-    close(905)
 
+   if (write_zonal_stats == 1) then
+        open(unit=901, file = trim(adjustL(grid_data_file) ))
+        open(unit=902, file = trim(adjustL(grid_pct_edge_file) ))
+        open(unit=903, file = trim(adjustL(grid_depth_file_Gdw) ))
+        open(unit=904, file = trim(adjustL(grid_depth_file_GwT) ))
+        open(unit=905, file = trim(adjustL(grid_depth_file_MtD) ))
+        write(901,'(A)') 'GRID,MEAN_BED_ELEV,MEAN_LAND_ELEV,PERCENT_LAND_0-100,PERCENT_WETLAND_0-100,PERCENT_WATER_0-100'
+        write(902,'(A)') 'GRID,PERCENT_EDGE_0-100'
+        write(903,'(A)') 'GRID_ID,VALUE_0,VALUE_4,VALUE_8,VALUE_12,VALUE_18,VALUE_22,VALUE_28,VALUE_32,VALUE_36,VALUE_40,VALUE_44,VALUE_78,VALUE_150,VALUE_151'
+        write(904,'(A)') 'GRID_ID,VALUE_0,VALUE_6,VALUE_18,VALUE_22,VALUE_26,VALUE_30,VALUE_34,VALUE_100,VALUE_101'
+        write(905,'(A)') 'GRID_ID,VALUE_0,VALUE_8,VALUE_30,VALUE_36,VALUE_42,VALUE_46,VALUE_50,VALUE_56,VALUE_57'
     
-    ! write debug file that prints out number of DEM pixels in each ICM-LAVegMod grid cell
-    open(unit=9050, file = 'grid_ndem.csv')    
-    write(9050,*) 'CELL_ID,ndem_pixels'
-    do i = 1,ngrid
-        write(9050,'(I0,A,I0)') i,',',grid_ndem_all(i)
-    end do
-    close(9050)
+        do i = 1,ngrid
+            c = grid_comp(i)
+            ! calculate some local tabulations on land area to be used in output files
+            grid_pct_vg_land = dem_NoDataVal
+            grid_pct_land_tot = dem_NoDataVal
+            grid_pct_land_wetl = dem_NoDataVal
     
-    ! write end-of-year ICM-Hydro compartment elevation data file    
-    write(  *,*) ' - writing end-of-year compartment data files'
-    write(000,*) ' - writing end-of-year compartment data files'
+            grid_pct_vg_land   = 1.0 - grid_pct_water(i) - grid_pct_flt(i) - grid_pct_bare(i) - grid_pct_upland(i)
+            grid_pct_land_tot  = 1.0 - grid_pct_water(i) - grid_pct_flt(i)
+            grid_pct_land_wetl =       grid_pct_vg_land  + grid_pct_flt(i) + grid_pct_bare(i)
     
-    open(unit=906, file = trim(adjustL(comp_elev_file)))
-    open(unit=907, file = trim(adjustL(comp_wat_file)))
-    open(unit=908, file = trim(adjustL(comp_upl_file)))
-
-    ! write headers
-    write(906,'(A)') 'ICM_ID,MEAN_BED_ELEV,MEAN_MARSH_ELEV,MARSH_EDGE_AREA'
-    !write(907,*) 'there is no header for this file'
-    !write(908,*) 'there is no header for this file'
-
-    do i = 1,ncomp
+            ! Grid data file passed into ICM-Hydro reports percentages from 0-100 instead of 0-1
+            write(901,1901) i,                          &
+   &                    grid_bed_z(i),                  &
+   &                    grid_land_z(i),                 &
+   &                    100.0*grid_pct_land_tot,        &
+   &                    100.0*grid_pct_land_wetl,       &
+   &                    100.0*grid_pct_water(i)
+            
+            ! Grid based edge file passed into ICM-HSI reports percentages from 0-100 instead of 0-1    
+            write(902,1902) i,                          &
+   &                    100.0*grid_pct_edge(i)
+            
+            write(903,1903) i,                          &
+   &                    grid_gadwl_dep(i,1),            &
+   &                    grid_gadwl_dep(i,2),            &
+   &                    grid_gadwl_dep(i,3),            &
+   &                    grid_gadwl_dep(i,4),            &
+   &                    grid_gadwl_dep(i,5),            &
+   &                    grid_gadwl_dep(i,6),            &
+   &                    grid_gadwl_dep(i,7),            &
+   &                    grid_gadwl_dep(i,8),            &
+   &                    grid_gadwl_dep(i,9),            &
+   &                    grid_gadwl_dep(i,10),           &
+   &                    grid_gadwl_dep(i,11),           &
+   &                    grid_gadwl_dep(i,12),           &
+   &                    grid_gadwl_dep(i,13),           &
+   &                    grid_gadwl_dep(i,14)
+            
+            write(904,1904) i,                          &
+   &                    grid_gwteal_dep(i,1),           &
+   &                    grid_gwteal_dep(i,2),           &
+   &                    grid_gwteal_dep(i,3),           &
+   &                    grid_gwteal_dep(i,4),           &
+   &                    grid_gwteal_dep(i,5),           &
+   &                    grid_gwteal_dep(i,6),           &
+   &                    grid_gwteal_dep(i,7),           &
+   &                    grid_gwteal_dep(i,8),           &
+   &                    grid_gwteal_dep(i,9)
+            
+            write(905,1904) i,                          &
+   &                    grid_motduck_dep(i,1),          &
+   &                    grid_motduck_dep(i,2),          &
+   &                    grid_motduck_dep(i,3),          &
+   &                    grid_motduck_dep(i,4),          &
+   &                    grid_motduck_dep(i,5),          &
+   &                    grid_motduck_dep(i,6),          &
+   &                    grid_motduck_dep(i,7),          &
+   &                    grid_motduck_dep(i,8),          &
+   &                    grid_motduck_dep(i,9)
         
-        write(906,1906) i,                          &
-   &                comp_water_z(i),                &
-   &                comp_wetland_z(i),              &
-   &                comp_edge_area(i)
+        end do
         
-        write(907,1907) i,                          &
-   &                comp_pct_water(i)
+        close(901)
+        close(902)
+        close(903)
+        close(904)
+        close(905)
+    
+        ! write end-of-year ICM-Hydro compartment elevation data file    
+        write(  *,*) ' - writing end-of-year compartment data files'
+        write(000,*) ' - writing end-of-year compartment data files'
         
-        write(908,1907) i,                          &
-   &                comp_pct_upland(i)
-
-    end do
+        open(unit=906, file = trim(adjustL(comp_elev_file)))
+        open(unit=907, file = trim(adjustL(comp_wat_file)))
+        open(unit=908, file = trim(adjustL(comp_upl_file)))
     
-    close(906)
-    close(907)
-    close(908)
+        ! write headers
+        write(906,'(A)') 'ICM_ID,MEAN_BED_ELEV,MEAN_MARSH_ELEV,MARSH_EDGE_AREA'
+        !write(907,*) 'there is no header for this file'
+        !write(908,*) 'there is no header for this file'
     
-    ! write debug file that prints out number of DEM pixels in each ICM-Hydro compartment
-    open(unit=9080, file = 'comp_ndem.csv')    
-    write(9080,*) 'ICM_ID,ndem_pixels'
-    do i = 1,ncomp
-        write(9080,'(I0,A,I0)') i,',',comp_ndem_all(i)
-    end do
-    close(9080)
+        do i = 1,ncomp
+            write(906,1906) i,                          &
+   &                    comp_water_z(i),                &
+   &                    comp_wetland_z(i),              &
+   &                    comp_edge_area(i)
+            
+            write(907,1907) i,                          &
+   &                    comp_pct_water(i)
+            
+            write(908,1907) i,                          &
+   &                    comp_pct_upland(i)
+        end do
+        
+        close(906)
+        close(907)
+        close(908)
+        
+    else
+        ! write output CSV file at DEM resolution to be used by external zonal statistics scripts
+        open(unit=910, file = trim(adjustL(dem_grid_out_summary_file) ) )
+        write(910,'(A)') 'ndem,ICM_LAVegMod_GridCell,ICM_Hydro_Compartment,landtype,edge,z_NAVD88_m'
+        do i = 1,ndem
+            write(910,1910) i,dem_grid(i),dem_comp(i),dem_lndtyp(i),dem_edge(i),dem_z(i)
+        end do
+        close(910)
+ 
+    end if
+    
     
     ! write ecoregion summary file (each year will be appended to bottom of file)
     open(unit=909, file = 'geomorph/output/'//trim(adjustL(fnc_tag))//'_land_veg.csv',position='append')
@@ -200,7 +208,26 @@ subroutine write_output_summaries
     end do
     
     close(909)
+
     
+    if (write_debug == 1) then
+        ! write debug file that prints out number of DEM pixels in each ICM-LAVegMod grid cell
+        open(unit=9050, file = 'grid_ndem.csv')    
+        write(9050,*) 'CELL_ID,ndem_pixels'
+        do i = 1,ngrid
+            write(9050,'(I0,A,I0)') i,',',grid_ndem_all(i)
+        end do
+        close(9050)
+    
+        ! write debug file that prints out number of DEM pixels in each ICM-Hydro compartment
+        open(unit=9080, file = 'comp_ndem.csv')    
+        write(9080,*) 'ICM_ID,ndem_pixels'
+        do i = 1,ncomp
+            write(9080,'(I0,A,I0)') i,',',comp_ndem_all(i)
+        end do
+        close(9080)
+    end if
+
 1900    format(I0,',',I0,19(',',F0.4),',',F0.2)
 1901    format(I0,2(',',F0.4),3(',',F0.2))
 1902    format(I0,',',F0.2)  
@@ -209,7 +236,7 @@ subroutine write_output_summaries
 1906    format(I0,2(',',F0.4),',',I0)
 1907    format(I0,',',F0.4)  
 1909    format(2(A,','),I,',',2(A,','),I)
-    
+1910    format( 5(I0,','), F0.4 )       
     
         
         
